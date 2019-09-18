e70d3e2ea486210ae6796517b39ef9f0301a6e04
Fatih Rahman Mert <fatihmert3858@gmail.com>
2019-09-18 13:21:50 +0300

    Rabbitmq Example Projects

---
+++ Consumer/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("rabbit")]
+        public ActionResult<string> GetRabbit()
+        {
+            string result = "";
+            var factory = new ConnectionFactory {HostName = "localhost", UserName = "admin", Password = "123456"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("basic", false, false, true, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    result = message;
+                    Console.WriteLine($"Message => {message}");
+                };
+
+                channel.BasicConsume("basic", true, consumer);
+            }
+
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory{ HostName="localhost", UserName = "admin", Password = "123456"};
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("basic",
+                                    false,
+                                    false,
+                                    true,
+                                    null);
+                
+                var message = new {name = "Fatih", surname = "Mert", email = "fatihmert3858@gmail.com",
+                city = "Kayseri"};
+
+                var json = JsonConvert.SerializeObject(message);
+
+                var body = Encoding.UTF8.GetBytes(json);
+
+                channel.BasicPublish("", "basic", null, body);
+            }
+        }
+    }
+}
